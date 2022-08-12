19bcced58224d142b88eb63bb3aea8ab97e75036
sjseo85 <sjseo85@gmail.com>
2022-08-13 02:32:03 +0900

    msa

---
+++ src/main/java/com/begin_devops/firstmsa/FirstmsaApplication.java
@@ -0,0 +1,13 @@
+package com.begin_devops.firstmsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstmsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstmsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/begin_devops/firstmsa/FirstmsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begin_devops.firstmsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstmsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
